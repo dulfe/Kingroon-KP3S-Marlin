82241a9b5cd78f29c8e7b7d748ab2f7de204f6c6
Daniel Ulfe <dulfe.github@outlook.com>
2020-10-16 20:05:41 -0400

    Replaced stepper driver from stock to TMC2208 STANDALONE (TMC2225 is a drop-in replacement to TMC2208) Rollback CENTER HOME fix because marlin 2.0.7.2 seem to calculate it correctly now.

--- Configuration.h
+++ Configuration.h
@@ -683,15 +683,15 @@
  *          TMC5130, TMC5130_STANDALONE, TMC5160, TMC5160_STANDALONE
  * :['A4988', 'A5984', 'DRV8825', 'LV8729', 'L6470', 'L6474', 'POWERSTEP01', 'TB6560', 'TB6600', 'TMC2100', 'TMC2130', 'TMC2130_STANDALONE', 'TMC2160', 'TMC2160_STANDALONE', 'TMC2208', 'TMC2208_STANDALONE', 'TMC2209', 'TMC2209_STANDALONE', 'TMC26X', 'TMC26X_STANDALONE', 'TMC2660', 'TMC2660_STANDALONE', 'TMC5130', 'TMC5130_STANDALONE', 'TMC5160', 'TMC5160_STANDALONE']
  */
-#define X_DRIVER_TYPE  A4988
-#define Y_DRIVER_TYPE  A4988
-#define Z_DRIVER_TYPE  A4988
+#define X_DRIVER_TYPE  TMC2208_STANDALONE
+#define Y_DRIVER_TYPE  TMC2208_STANDALONE
+#define Z_DRIVER_TYPE  TMC2208_STANDALONE
 //#define X2_DRIVER_TYPE A4988
 //#define Y2_DRIVER_TYPE A4988
 //#define Z2_DRIVER_TYPE A4988
 //#define Z3_DRIVER_TYPE A4988
 //#define Z4_DRIVER_TYPE A4988
-#define E0_DRIVER_TYPE A4988
+#define E0_DRIVER_TYPE TMC2208_STANDALONE
 //#define E1_DRIVER_TYPE A4988
 //#define E2_DRIVER_TYPE A4988
 //#define E3_DRIVER_TYPE A4988
@@ -1393,12 +1393,12 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  //#define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
-  //#define Z_SAFE_HOMING_Y_POINT Y_CENTER  // Y point for Z homing
+  #define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
+  #define Z_SAFE_HOMING_Y_POINT Y_CENTER  // Y point for Z homing
 
-  constexpr float z_safe_homing_nozzle_to_probe_offset[] = NOZZLE_TO_PROBE_OFFSET;
-  #define Z_SAFE_HOMING_X_POINT X_CENTER - z_safe_homing_nozzle_to_probe_offset[0]  // Adjust center based on PROBE LOCATION X
-  #define Z_SAFE_HOMING_Y_POINT Y_CENTER - z_safe_homing_nozzle_to_probe_offset[1]  // Adjust center based on PROBE LOCATION Y
+  //constexpr float z_safe_homing_nozzle_to_probe_offset[] = NOZZLE_TO_PROBE_OFFSET;
+  //#define Z_SAFE_HOMING_X_POINT X_CENTER - z_safe_homing_nozzle_to_probe_offset[0]  // Adjust center based on PROBE LOCATION X
+  //#define Z_SAFE_HOMING_Y_POINT Y_CENTER - z_safe_homing_nozzle_to_probe_offset[1]  // Adjust center based on PROBE LOCATION Y
 #endif
 
 // Homing speeds (mm/min)
