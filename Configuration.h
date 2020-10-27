f30c76a838e6cfcab2123f97038bf88cc2407c58
Daniel Ulfe <dulfe.github@outlook.com>
2020-10-27 15:39:07 -0400

    Increased UBL Grid points.

--- Configuration.h
+++ Configuration.h
@@ -1319,7 +1319,7 @@
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
   #define MESH_INSET 10              // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 4      // Don't use more than 15 points per axis, implementation limited.
+  #define GRID_MAX_POINTS_X 5      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
